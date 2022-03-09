747f4d7a7997e64095d902798870f23ee8138fcf
hhimanshu <dev.atwork@yahoo.com>
2022-03-09 16:34:26 +0530

    create post and relationship with user via author field

--- keystone.ts
+++ keystone.ts
@@ -1,17 +1,11 @@
-import {config, list} from "@keystone-6/core";
-import {text} from "@keystone-6/core/fields";
+import {config} from "@keystone-6/core";
+import {lists} from "./schema";
+
 
 export default config({
     db: {
         provider: "sqlite",
         url: "file:./keystone.db",
     },
-    lists: {
-        User: list({
-            fields: {
-                name: text({validation: {isRequired: true}}),
-                email: text({validation: {isRequired: true}, isIndexed: "unique"})
-            }
-        })
-    }
+    lists
 });
